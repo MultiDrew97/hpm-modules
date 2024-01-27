3c645d916e81d0623a82321b6ffc35314162f02c
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2024-01-27 17:11:56 -0600

    completed generification

--- core/lib/models/password.ts
+++ core/lib/models/password.ts
@@ -1,119 +1,119 @@
-import { IEntry } from '../utils/interfaces'
-import { Model, model, Schema, SchemaTypes, Document } from 'mongoose'
-import { User } from './user'
+// import { IEntry } from '../utils/interfaces'
+// import { Model, model, Schema, SchemaTypes, Document } from 'mongoose'
+// import { User } from './user'
 
-export const CollectionName = {
-	name: 'Entry',
-	collection: 'Entries',
-}
+// export const CollectionName = {
+// 	name: 'Entry',
+// 	collection: 'Entries',
+// }
 
-export interface IPassEntryDoc extends IEntry, Document {
-	/**
-	 * Update the password stored for the entry
-	 * @param newPassword The new password to update to
-	 */
-	updatePassword(newPassword: string): Promise<any>
+// export interface IPassEntryDoc extends IEntry, Document {
+// 	/**
+// 	 * Update the password stored for the entry
+// 	 * @param newPassword The new password to update to
+// 	 */
+// 	updatePassword(newPassword: string): Promise<any>
 
-	/**
-	 * Update the TOTP key for the specified entry
-	 * @param totpKey The key to update to
-	 */
-	updateTotp(totpKey: string): Promise<any>
-}
+// 	/**
+// 	 * Update the TOTP key for the specified entry
+// 	 * @param totpKey The key to update to
+// 	 */
+// 	updateTotp(totpKey: string): Promise<any>
+// }
 
-interface IPassEntryModel extends Model<IPassEntryDoc> {
-	/**
-	 * Create a new password entry and add to user's list
-	 * @param userID ID of user to add to
-	 * @param newEntry New entry to add
-	 */
-	addEntry(userID: string, newEntry: IEntry): Promise<boolean>
-}
+// interface IPassEntryModel extends Model<IPassEntryDoc> {
+// 	/**
+// 	 * Create a new password entry and add to user's list
+// 	 * @param userID ID of user to add to
+// 	 * @param newEntry New entry to add
+// 	 */
+// 	addEntry(userID: string, newEntry: IEntry): Promise<boolean>
+// }
 
-const entrySchema: Schema<IPassEntryDoc> = new Schema({
-	name: {
-		type: SchemaTypes.String,
-		default: '',
-	},
-	sites: [
-		{
-			type: SchemaTypes.String,
-			default: '',
-		},
-	],
-	login: {
-		username: {
-			type: SchemaTypes.String,
-			default: '',
-		},
-		password: {
-			type: SchemaTypes.String,
-			default: '',
-		},
-		salt: {
-			type: SchemaTypes.String,
-			default: '',
-		},
-	},
-	passwordHistory: [
-		{
-			type: SchemaTypes.String,
-			alias: 'history',
-			default: '',
-		},
-	],
-	totpKey: {
-		type: SchemaTypes.String,
-		alias: 'key',
-		default: '',
-	},
-})
+// const entrySchema: Schema<IPassEntryDoc> = new Schema({
+// 	name: {
+// 		type: SchemaTypes.String,
+// 		default: '',
+// 	},
+// 	sites: [
+// 		{
+// 			type: SchemaTypes.String,
+// 			default: '',
+// 		},
+// 	],
+// 	login: {
+// 		username: {
+// 			type: SchemaTypes.String,
+// 			default: '',
+// 		},
+// 		password: {
+// 			type: SchemaTypes.String,
+// 			default: '',
+// 		},
+// 		salt: {
+// 			type: SchemaTypes.String,
+// 			default: '',
+// 		},
+// 	},
+// 	passwordHistory: [
+// 		{
+// 			type: SchemaTypes.String,
+// 			alias: 'history',
+// 			default: '',
+// 		},
+// 	],
+// 	totpKey: {
+// 		type: SchemaTypes.String,
+// 		alias: 'key',
+// 		default: '',
+// 	},
+// })
 
-/*entrySchema.pre('save', function() {
-	if (!this.isNew)
-		return
+// /*entrySchema.pre('save', function() {
+// 	if (!this.isNew)
+// 		return
 
-	// this.login.salt = (new Types.ObjectId()).toString()
-	console.debug(this.login.password)
-	this.login.password = encrypt(this.login.password, this.login.salt)
-	console.debug(this.login.password)
-})*/
+// 	// this.login.salt = (new Types.ObjectId()).toString()
+// 	console.debug(this.login.password)
+// 	this.login.password = encrypt(this.login.password, this.login.salt)
+// 	console.debug(this.login.password)
+// })*/
 
-entrySchema.static('addEntry', function (userID: string, newEntry: IEntry): Promise<any> {
-	return PassEntry.create(newEntry)
-		.then((password) => {
-			return User.addEntry(userID, password.id!)
-		})
-		.catch((reason) => {
-			return Promise.reject(reason)
-		})
-})
+// entrySchema.static('addEntry', function (userID: string, newEntry: IEntry): Promise<any> {
+// 	return PassEntry.create(newEntry)
+// 		.then((password) => {
+// 			return User.addEntry(userID, password.id!)
+// 		})
+// 		.catch((reason) => {
+// 			return Promise.reject(reason)
+// 		})
+// })
 
-entrySchema.method('updatePassword', function (newPassword: string): Promise<any> {
-	if (this.passwordHistory.length >= 5) this.passwordHistory.shift()
+// entrySchema.method('updatePassword', function (newPassword: string): Promise<any> {
+// 	if (this.passwordHistory.length >= 5) this.passwordHistory.shift()
 
-	this.passwordHistory.push(this.login.password)
+// 	this.passwordHistory.push(this.login.password)
 
-	// TODO: Is this encrypted before getting here?
-	this.login.password = newPassword
+// 	// TODO: Is this encrypted before getting here?
+// 	this.login.password = newPassword
 
-	return this.save()
-})
+// 	return this.save()
+// })
 
-entrySchema.method('updateTotp', function (totpKey: string): Promise<any> {
-	this.totpKey = totpKey
-	return this.save()
-})
+// entrySchema.method('updateTotp', function (totpKey: string): Promise<any> {
+// 	this.totpKey = totpKey
+// 	return this.save()
+// })
 
-/*function length(arr: (string | IPasswordEntryDoc)[]) {
-	return arr.length <= 5
-}*/
+// /*function length(arr: (string | IPasswordEntryDoc)[]) {
+// 	return arr.length <= 5
+// }*/
 
-/**
- * Password model
- */
-export const PassEntry = model<IPassEntryDoc, IPassEntryModel>(
-	CollectionName.name,
-	entrySchema,
-	CollectionName.collection
-)
+// /**
+//  * Password model
+//  */
+// export const PassEntry = model<IPassEntryDoc, IPassEntryModel>(
+// 	CollectionName.name,
+// 	entrySchema,
+// 	CollectionName.collection
+// )
--- core/lib/models/user.ts
+++ core/lib/models/user.ts
@@ -1,240 +1,239 @@
-import { IAuth, IEntry, IUser, IUserConfig } from '../utils/interfaces'
-import { model, Model, Schema, SchemaTypes, Types, Document } from 'mongoose'
-import { encrypt } from '../utils/functions'
-import { DEFAULT_CONFIG } from '../utils/constants'
-import { ArgumentError, DatabaseError, LoginError } from '../utils/errors'
-import { PassEntry, CollectionName as passName } from './password'
+// import { IAuth } from '../utils/interfaces'
+// import { model, Model, Schema, SchemaTypes, Types, Document } from 'mongoose'
+// import { encrypt } from '../utils/functions'
+// import { ArgumentError, DatabaseError } from '../utils/errors'
+// import { PassEntry, CollectionName as passName } from './password'
 
-const CollectionName = {
-	name: 'User',
-	collection: 'Users',
-}
+// const CollectionName = {
+// 	name: 'User',
+// 	collection: 'Users',
+// }
 
-export interface IUserDoc extends IUser, Document {}
+// export interface IUserDoc extends IUser, Document {}
 
-interface IUserModel extends Model<IUserDoc> {
-	/**
-	 * Attempt to log in using the provided credentials
-	 *
-	 * @param auth The credentials for the user attempting to login
-	 */
-	login(auth: IAuth): Promise<IUser>
+// interface IUserModel extends Model<IUserDoc> {
+// 	/**
+// 	 * Attempt to log in using the provided credentials
+// 	 *
+// 	 * @param auth The credentials for the user attempting to login
+// 	 */
+// 	login(auth: IAuth): Promise<IUser>
 
-	/**
-	 * Check the password is valid for specified user
-	 * @param id ID of desired user
-	 * @param password The password to be checked
-	 */
-	checkPassword(id: string, password: string): Promise<boolean>
+// 	/**
+// 	 * Check the password is valid for specified user
+// 	 * @param id ID of desired user
+// 	 * @param password The password to be checked
+// 	 */
+// 	checkPassword(id: string, password: string): Promise<boolean>
 
-	/**
-	 * Verifies if username provided is unique
-	 * @param username Username to check for
-	 */
-	isUniqueUsername(username: string): Promise<boolean>
+// 	/**
+// 	 * Verifies if username provided is unique
+// 	 * @param username Username to check for
+// 	 */
+// 	isUniqueUsername(username: string): Promise<boolean>
 
-	/**
-	 * Get the salt for the specified user
-	 * @param id The ID of the user to get the salt from
-	 */
-	getSalt(id: string): Promise<string>
+// 	/**
+// 	 * Get the salt for the specified user
+// 	 * @param id The ID of the user to get the salt from
+// 	 */
+// 	getSalt(id: string): Promise<string>
 
-	/**
-	 * Add a new entry for the specified user
-	 * @param userID The ID of the user to add the new entry to
-	 * @param entryID The ID of the new entry to add
-	 */
-	addEntry(userID: string, entryID: string): Promise<any>
+// 	/**
+// 	 * Add a new entry for the specified user
+// 	 * @param userID The ID of the user to add the new entry to
+// 	 * @param entryID The ID of the new entry to add
+// 	 */
+// 	addEntry(userID: string, entryID: string): Promise<any>
 
-	/**
-	 * Remove specified entry from specified user's entry list
-	 * @param userID The ID of the user to remove the entry from
-	 * @param entryID The ID of the entry to remove
-	 */
-	removeEntry(userID: string, entryID: string): Promise<boolean>
+// 	/**
+// 	 * Remove specified entry from specified user's entry list
+// 	 * @param userID The ID of the user to remove the entry from
+// 	 * @param entryID The ID of the entry to remove
+// 	 */
+// 	removeEntry(userID: string, entryID: string): Promise<boolean>
 
-	/**
-	 * Update a user's master password
-	 * @param userID The ID of the user to change master password
-	 * @param newPassword The new password to change master to
-	 */
-	updatePassword(userID: string, newPassword: string): Promise<boolean>
+// 	/**
+// 	 * Update a user's master password
+// 	 * @param userID The ID of the user to change master password
+// 	 * @param newPassword The new password to change master to
+// 	 */
+// 	updatePassword(userID: string, newPassword: string): Promise<boolean>
 
-	/**
-	 * Get the config for the specified users
-	 * @param userID The ID of the user whose config to get
-	 */
-	getUserConfig(userID: string): Promise<IUserConfig>
+// 	/**
+// 	 * Get the config for the specified users
+// 	 * @param userID The ID of the user whose config to get
+// 	 */
+// 	getUserConfig(userID: string): Promise<IUserConfig>
 
-	/**
-	 * Update the config of a desired user
-	 * @param userID The ID of the user to update their config
-	 * @param newConfig The new data for the config
-	 */
-	updateUserConfig(userID: string, newConfig: IUserConfig): Promise<boolean>
-}
+// 	/**
+// 	 * Update the config of a desired user
+// 	 * @param userID The ID of the user to update their config
+// 	 * @param newConfig The new data for the config
+// 	 */
+// 	updateUserConfig(userID: string, newConfig: IUserConfig): Promise<boolean>
+// }
 
-const userSchema: Schema<IUserDoc> = new Schema({
-	name: {
-		type: SchemaTypes.String,
-		default: 'John Doe',
-	},
-	entries: [
-		{
-			type: SchemaTypes.ObjectId,
-			unique: true,
-			ref: passName.name,
-		},
-	],
-	config: {
-		type: Object,
-		default: DEFAULT_CONFIG,
-	},
-	login: {
-		username: {
-			type: SchemaTypes.String,
-			unique: true,
-			required: true,
-		},
-		password: {
-			type: SchemaTypes.String,
-			required: true,
-		},
-		salt: {
-			type: SchemaTypes.String,
-			default: new Types.ObjectId().toString(),
-		},
-	},
-})
+// const userSchema: Schema<IUserDoc> = new Schema({
+// 	name: {
+// 		type: SchemaTypes.String,
+// 		default: 'John Doe',
+// 	},
+// 	entries: [
+// 		{
+// 			type: SchemaTypes.ObjectId,
+// 			unique: true,
+// 			ref: passName.name,
+// 		},
+// 	],
+// 	config: {
+// 		type: Object,
+// 		default: DEFAULT_CONFIG,
+// 	},
+// 	login: {
+// 		username: {
+// 			type: SchemaTypes.String,
+// 			unique: true,
+// 			required: true,
+// 		},
+// 		password: {
+// 			type: SchemaTypes.String,
+// 			required: true,
+// 		},
+// 		salt: {
+// 			type: SchemaTypes.String,
+// 			default: new Types.ObjectId().toString(),
+// 		},
+// 	},
+// })
 
-userSchema.pre('save', function () {
-	if (!this.isNew) return
+// userSchema.pre('save', function () {
+// 	if (!this.isNew) return
 
-	// this.login.salt = (new Types.ObjectId()).toString()
-	this.login.password = encrypt(this.login.password!, this.login.salt)
-})
+// 	// this.login.salt = (new Types.ObjectId()).toString()
+// 	this.login.password = encrypt(this.login.password!, this.login.salt)
+// })
 
-userSchema.pre(/.*delete.*/i, function () {
-	// console.log(this.id)
-	// console.log(this.entries)
-	// for (let entry of this.entries) {
-	// 	PassEntry.findByIdAndDelete(entry)
-	// }
-})
+// userSchema.pre(/.*delete.*/i, function () {
+// 	// console.log(this.id)
+// 	// console.log(this.entries)
+// 	// for (let entry of this.entries) {
+// 	// 	PassEntry.findByIdAndDelete(entry)
+// 	// }
+// })
 
-/*userSchema.pre(/find.*!/, function() {
-	this.populate('entries')
-})*/
+// /*userSchema.pre(/find.*!/, function() {
+// 	this.populate('entries')
+// })*/
 
-userSchema.static('updatePassword', async function (userID: string, newPassword: string) {
-	return User.findById(userID)
-		.then((user) => {
-			if (!user) throw new DatabaseError(`Unknown userID ${userID}`)
+// userSchema.static('updatePassword', async function (userID: string, newPassword: string) {
+// 	return User.findById(userID)
+// 		.then((user) => {
+// 			if (!user) throw new DatabaseError(`Unknown userID ${userID}`)
 
-			user.login.password = encrypt(newPassword, user.login.salt)
-			return user.save()
-		})
-		.catch((reason) => {
-			return Promise.reject(reason)
-		})
-})
+// 			user.login.password = encrypt(newPassword, user.login.salt)
+// 			return user.save()
+// 		})
+// 		.catch((reason) => {
+// 			return Promise.reject(reason)
+// 		})
+// })
 
-userSchema.static('login', async function (auth: IAuth): Promise<IUser> {
-	// TODO: Determine what I actually need from the login
-	return User.findOne({ username: auth.username }, 'id login').then((user) => {
-		if (!user) throw new LoginError('Unknown username')
+// userSchema.static('login', async function (auth: IAuth): Promise<IUser> {
+// 	// TODO: Determine what I actually need from the login
+// 	return User.findOne({ username: auth.username }, 'id login').then((user) => {
+// 		if (!user) throw new LoginError('Unknown username')
 
-		if (user.login.password !== encrypt(auth.password!, user.login.salt)) throw new LoginError('Incorrect password')
+// 		if (user.login.password !== encrypt(auth.password!, user.login.salt)) throw new LoginError('Incorrect password')
 
-		return user
-	})
-})
+// 		return user
+// 	})
+// })
 
-userSchema.static('checkPassword', async function (userID: string, password: string): Promise<boolean> {
-	// TODO: Determine what I actually need from the login
-	let user = await User.findById(userID, 'id login')
+// userSchema.static('checkPassword', async function (userID: string, password: string): Promise<boolean> {
+// 	// TODO: Determine what I actually need from the login
+// 	let user = await User.findById(userID, 'id login')
 
-	if (!user) throw new LoginError(`Unknown userID ${userID}`)
+// 	if (!user) throw new LoginError(`Unknown userID ${userID}`)
 
-	if (user.login.password !== encrypt(password, user.login.salt)) throw new LoginError('Incorrect password')
+// 	if (user.login.password !== encrypt(password, user.login.salt)) throw new LoginError('Incorrect password')
 
-	return true
-})
+// 	return true
+// })
 
-userSchema.static('addEntry', async function (userID: string, entryID: string): Promise<any> {
-	return User.findById(userID).then((user: IUserDoc | null) => {
-		if (!user) throw new ArgumentError(`Unknown userID ${userID}`)
+// userSchema.static('addEntry', async function (userID: string, entryID: string): Promise<any> {
+// 	return User.findById(userID).then((user: IUserDoc | null) => {
+// 		if (!user) throw new ArgumentError(`Unknown userID ${userID}`)
 
-		user.entries.push(entryID)
-		return user.save()
-	})
-})
+// 		user.entries.push(entryID)
+// 		return user.save()
+// 	})
+// })
 
-userSchema.static('removeEntry', async function (userID: string, entryID: string): Promise<boolean> {
-	let removed: (IEntry | Types.ObjectId | string)[]
-	return User.findById(userID).then((user: IUserDoc | null) => {
-		if (!user) throw new ArgumentError(`Unknown userID ${userID}`)
+// userSchema.static('removeEntry', async function (userID: string, entryID: string): Promise<boolean> {
+// 	let removed: (IEntry | Types.ObjectId | string)[]
+// 	return User.findById(userID).then((user: IUserDoc | null) => {
+// 		if (!user) throw new ArgumentError(`Unknown userID ${userID}`)
 
-		let index = user.entries.indexOf(entryID)
-		/*let index = user.entries.findIndex((value, index, arr) => {
+// 		let index = user.entries.indexOf(entryID)
+// 		/*let index = user.entries.findIndex((value, index, arr) => {
 
-		})*/
+// 		})*/
 
-		if (index < 0) throw new ArgumentError(`Unknown entryID ${entryID} for userID ${userID}`)
+// 		if (index < 0) throw new ArgumentError(`Unknown entryID ${entryID} for userID ${userID}`)
 
-		removed = user.entries.splice(index, 1)
-		return user
-			.save()
-			.then((_) => {
-				return PassEntry.findByIdAndDelete(entryID).then((_) => {
-					return true
-				})
-			})
-			.catch(async (_) => {
-				if (removed.length > 0 && !user.entries.includes(removed[0])) {
-					user.entries.push(removed[0])
-					await user.save()
-				}
+// 		removed = user.entries.splice(index, 1)
+// 		return user
+// 			.save()
+// 			.then((_) => {
+// 				return PassEntry.findByIdAndDelete(entryID).then((_) => {
+// 					return true
+// 				})
+// 			})
+// 			.catch(async (_) => {
+// 				if (removed.length > 0 && !user.entries.includes(removed[0])) {
+// 					user.entries.push(removed[0])
+// 					await user.save()
+// 				}
 
-				return false
-			})
-	})
-})
+// 				return false
+// 			})
+// 	})
+// })
 
-userSchema.static('getSalt', async function (userID: any): Promise<string> {
-	return await User.findById(userID, 'login.salt').then((user) => {
-		if (!user) throw new DatabaseError(`Unknown userID ${userID}`)
+// userSchema.static('getSalt', async function (userID: any): Promise<string> {
+// 	return await User.findById(userID, 'login.salt').then((user) => {
+// 		if (!user) throw new DatabaseError(`Unknown userID ${userID}`)
 
-		return user.login.salt!
-	})
-})
+// 		return user.login.salt!
+// 	})
+// })
 
-userSchema.static('getUserConfig', async function (userID: string): Promise<IUserConfig> {
-	return await User.findById(userID, 'config').then((user) => {
-		if (!user) throw new DatabaseError(`Unknown userID ${userID}`)
+// userSchema.static('getUserConfig', async function (userID: string): Promise<IUserConfig> {
+// 	return await User.findById(userID, 'config').then((user) => {
+// 		if (!user) throw new DatabaseError(`Unknown userID ${userID}`)
 
-		return user.config
-	})
-})
+// 		return user.config
+// 	})
+// })
 
-userSchema.static('updateUserConfig', async function (userID: string, newConfig: IUserConfig) {
-	return User.findById(userID).then((user) => {
-		if (!user) throw new DatabaseError(`Unknown userID ${userID}`)
+// userSchema.static('updateUserConfig', async function (userID: string, newConfig: IUserConfig) {
+// 	return User.findById(userID).then((user) => {
+// 		if (!user) throw new DatabaseError(`Unknown userID ${userID}`)
 
-		user.config = newConfig
+// 		user.config = newConfig
 
-		return user.save()
-	})
-})
+// 		return user.save()
+// 	})
+// })
 
-userSchema.static('isUniqueUsername', async function (username: string): Promise<boolean> {
-	// MAYBE: find vs. findOne
-	return await User.find({ 'login.username': username }).then((user) => {
-		return user.length === 0
-	})
-})
+// userSchema.static('isUniqueUsername', async function (username: string): Promise<boolean> {
+// 	// MAYBE: find vs. findOne
+// 	return await User.find({ 'login.username': username }).then((user) => {
+// 		return user.length === 0
+// 	})
+// })
 
-/**
- * User model used to manage user's entries and user's data
- */
-export const User: IUserModel = model<IUserDoc, IUserModel>(CollectionName.name, userSchema, CollectionName.collection)
+// /**
+//  * User model used to manage user's entries and user's data
+//  */
+// export const User: IUserModel = model<IUserDoc, IUserModel>(CollectionName.name, userSchema, CollectionName.collection)
